7d865dc4fb3c07518e70c8603dc5b2fc2d7f2176
Younes Noufair <48910083+UNES97@users.noreply.github.com>
2024-10-04 19:19:03 +0100

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,11 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+		c.String(200, "Hello, Unes!")
+	})
+
+	router.GET("/bye", func(c *gin.Context) {
+		c.String(200, "Bye, Unes!")
 	})
 
 	router.Run(":8080")
